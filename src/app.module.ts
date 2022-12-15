85eaeaf0662d684a5d88e199f89f380c4f58d489
Fabrina <fabrina.nunes@hotmail.com>
2022-12-15 09:33:35 -0300

    feat: implement prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,12 @@
-import { SMTPMailService } from './mail/smtp-mail.service';
+import { PrismaService } from './prima.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MailService } from './mail/mail.service';
 
 // acoplador, ponto central pra acoplar os demais arquivos
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    {
-      provide: MailService,
-      useClass: SMTPMailService,
-    },
-  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
