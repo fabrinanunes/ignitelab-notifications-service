f603752feb6818fd8e1182d73069c8a0333b7556
Fabrina <fabrina.nunes@hotmail.com>
2022-12-15 09:33:26 -0300

    feat: implement prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { MailService } from './mail/mail.service';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prima.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
-@Controller()
+import { randomUUID } from 'crypto';
+
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly mailService: MailService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.mailService.sendEmail();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
